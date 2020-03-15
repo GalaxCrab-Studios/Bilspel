1aba26286d85b996a9dd4baa871fa3317579fc5e
galax-debug <arrrvid@live.se>
2020-03-15 15:24:53 +0100

    Velocity!

---
+++ bilspel/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject car;
+    
+
+    [SerializeField]
+    private float aheadSpeed,
+                  followDamping,
+                  cameraHeight;
+
+    private Vector3 velocity;
+    void Awake()
+    {
+        velocity = car.GetComponent<CarScript>().velocity;
+    }
+
+    void Update()
+    {
+        
+        Vector3 targetPosition = car.transform.position + Vector3.forward * cameraHeight + velocity * aheadSpeed;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followDamping * Time.deltaTime);
+    
+    }
+}
---
+++ bilspel/Assets/Scripts/CarScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    [Header("CalculateVelocity")]
+    [SerializeField]
+    private float enginePower, 
+                  drag, 
+                  mass, 
+                  rollingRes,
+                  brakeConst;
+
+    [Header("CalculateRotation")]
+    [SerializeField]
+    private float wheelDistance,
+                  turnSpeed
+                  ;
+
+
+    public Vector3 velocity;
+    Vector3 fDrag;
+    Vector3 rollingResistance;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        CalculateVelocity();
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    private void CalculateVelocity()
+    {
+
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 forward = transform.TransformDirection(Vector3.up);
+        Vector3 traction = forward * v * enginePower;
+        Vector3 brake = -forward * brakeConst;
+        Vector3 acceleration;
+
+        float curSpeed = velocity.magnitude;
+        Debug.Log(curSpeed);
+
+        rollingResistance = -rollingRes * velocity;
+        fDrag.x = -drag * velocity.x * curSpeed;
+        fDrag.y = -drag * velocity.y * curSpeed;
+        
+        if (v < 0)
+        {
+            acceleration = (brake + fDrag + rollingResistance) / mass;
+            
+        }
+        else
+        {
+            acceleration = (traction + fDrag + rollingResistance) / mass;
+           
+        }
+            velocity += acceleration * Time.deltaTime;
+    }
+
+    private void CalculateRotation()
+    {
+        float angle = 20;
+        float steer = wheelDistance / Mathf.Sin(angle);
+    }
+
+
+
+}
+    
